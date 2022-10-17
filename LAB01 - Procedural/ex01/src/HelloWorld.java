c775074b81e9572f3da6e717e0cf5bb3d24aa574
gabriel oliveira <zgabrieloliveir4@gmail.com>
2022-10-17 20:26:01 -0300

    Commit Exercícios

--- LAB01 - Procedural/ex01/src/HelloWorld.java
+++ LAB01 - Procedural/ex01/src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
         
+        System.out.println("Hello World!");
         System.out.println("Gabriel Antônio de Oliveira Leite");
         System.out.println("12111BSI222");
         
